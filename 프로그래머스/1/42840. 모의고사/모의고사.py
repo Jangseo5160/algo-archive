16d7645f2aa593aeb53c6ef70ad3f95a8e9bd46b
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-05-18 13:17:40 +0900

    [level 1] Title: 모의고사, Time: 2.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,20 +3,19 @@ def solution(answers):
     a1 = [1, 2, 3, 4, 5]
     a2 = [2, 1, 2, 3, 2, 4, 2, 5]
     a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    p1, p2, p3=0,0,0
-    
+    result=[0,0,0]
+
 
     for i in range(len(answers)):
         if a1[i%5] == answers[i]:
-            p1+=1
+            result[0]+=1
         if a2[i%8] == answers[i]:
-            p2+=1
+            result[1]+=1
         if a3[i%10] == answers[i]:
-            p3+=1
+            result[2]+=1
             
+    max_score = max(result)
     
-    max_score = max(p1,p2,p3)
-    result=[p1,p2,p3]
     for a in range(3):
         if result[a] == max_score:
             answer.append(a+1)
