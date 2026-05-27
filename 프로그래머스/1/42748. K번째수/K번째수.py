c7be17e601462aef82a744dfd1595f6cf44118e0
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-05-27 09:11:39 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,8 @@
 def solution(array, commands):
     answer = []
-    new_list=[]
-    for start, end, th in commands:
-        # print(start, end, th)
-        new_list=array[start-1:end]
-        # print(new_list)
-        new_list.sort()
-        # print(new_list[th-1])
-        answer.append(new_list[th-1])
+    temp=[]
+    for i, j, k in commands:
+        temp = sorted(array[i-1:j])
+        answer.append(temp[k-1])
+        temp.clear()
     return answer
