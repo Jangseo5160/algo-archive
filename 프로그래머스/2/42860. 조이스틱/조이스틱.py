9c08ba0c4107ff8f83329abad21140f7e5c3ed5a
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-05-28 19:28:19 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,23 +1,25 @@
 def solution(name):
     answer = 0
-    cnt=0
-    n=len(name)
+    up_down = 0
     
-    up_down = sum((min(ord(c) - ord('A'), 26-(ord(c) - ord('A'))) for c in name))
-    left_right = n-1  
-    
-    for i in range(n):
-        next_i = i+1
-        while next_i < n and name[next_i] == 'A':
-            next_i +=1
+    for c in name:
+        up=ord(c) - ord('A')
+        down= ord('Z') - ord(c)+1
+        up_down += min(up, down)
+        # print(up_down)
         
-        go_right_back = i+i+(n-next_i)
-        go_left_back = i + (n-next_i) + (n-next_i)
+    idx = 0
+    left_right = len(name)-1
 
-        left_right = min(left_right, go_right_back, go_left_back)
+        
+    for i in range(len(name)):
+        next_idx = i+1
+        while  next_idx<len(name) and name[next_idx] =='A':
+            next_idx+=1
+                    
+        left_back= (len(name)-next_idx)*2 + i
+        right_back = i + i + len(name)-next_idx
+        print(left_back, right_back, left_right)
+        left_right = min(left_back, right_back, left_right)
     
     return up_down + left_right
-
-# ord() => 아스키값 변환 후 사용
-# JAAAANN
-# AJAZ
