9a297f8decad927d3ad46ffd08ba04c76fb742d8
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-05-28 18:58:47 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,16 +1,14 @@
 def solution(n, lost, reserve):
     answer = 0
-    can_reserve = 0
     
     real_lost = set(lost) - set(reserve)
     real_reserve = set(reserve) - set(lost)
     
-    for i in sorted(real_lost):
-        if i-1 in real_reserve:
-            real_reserve.remove(i-1)
-            can_reserve+=1
-        elif i+1 in real_reserve:
-            real_reserve.remove(i+1)
-            can_reserve+=1
+    for i in sorted(real_reserve):
+        if i-1 in real_lost:
+            real_lost.remove(i-1)
+        elif i+1 in real_lost:
+            real_lost.remove(i+1)
             
-    return n - len(real_lost) + can_reserve
+        
+    return n-len(real_lost)
