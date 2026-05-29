2093da8a6220c2b9dabf615f58fe4ffc4340c35c
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-05-29 17:44:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.50 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,34 @@
 from collections import deque
-
+# bfs => queue
 def solution(maps):
     answer = 0
+    
     n=len(maps)
     m=len(maps[0])
     
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
     
-    queue=deque([(0,0)])
+    queue = deque()
+    queue.append((0,0,1))
+    
+    visited = [[False]*m for _ in range(n)]
+    visited[0][0]=True
     
     while queue:
-        x,y=queue.popleft()
+        x,y,dist = queue.popleft()
+        
+        if x==n-1 and y==m-1:
+            return dist
         
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if 0<=nx<n and 0<=ny<m and maps[nx][ny]==1:
-                maps[nx][ny]=maps[x][y]+1
-                queue.append((nx, ny))
+            nx = x + dx[i]
+            ny = y + dy[i]
             
-    answer=maps[n-1][m-1]
+            if 0<= nx< n and 0<=ny<m and not visited[nx][ny] and maps[nx][ny] ==1:
+                visited[nx][ny]=True
+                queue.append((nx, ny, dist +1))
     
-    return answer if answer > 1 else -1
-            
+    
+    
+    return -1
