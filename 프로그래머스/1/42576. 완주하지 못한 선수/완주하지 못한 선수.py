12ffc25e6dcb052d34e52f03efc33dd9d5e38d5c
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-07-25 00:06:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.75 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,10 +2,20 @@ from collections import *
 
 def solution(participant, completion):
     answer = ''
-    c = Counter(e for e in completion)
-    p = Counter(e for e in participant)
-    result = (p- c)
-    # print(result)
-    answer = list(result.keys())[0]
-    # print(answer)
-    return answer
+    # c = Counter(e for e in completion)
+    # p = Counter(e for e in participant)
+    # result = (p- c)
+    # # print(result)
+    # answer = list(result.keys())[0]
+    # # print(answer)
+    dic = {}
+    for e in participant:
+        dic[e]= dic.get(e, 0)+1
+    
+    for a in completion:
+        dic[a]-=1
+    
+    for name in dic:
+        if dic[name]>0:
+            return name
+    
