d5142a08238388921f00d2cab719267da68f51f9
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-08-12 15:40:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.36 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,31 +1,35 @@
 import heapq
 
+# 요청 시각 순으로 정렬
+# 절대 시간까지 도착한 작업들 후보에 추가 => 시간-시각-번호 순으로 정렬
+# 가장 짧은 처리 시간 먼저 처리
+# 작업 수행 후 절대시각-요청시각 누적합
+# 다음 절대시간부터 다시 도착한 작업들 후보에 추가
+
 def solution(jobs):
     joblist = []
     for idx, [req, dur] in enumerate(jobs):
-        joblist.append((req,dur, idx))
+        joblist.append((req,dur,idx))
     joblist.sort()
     
     answer = 0
-    n = len(jobs)
-    heap = []
+    heap = [] # 대기열
     i=0
     time = 0
     finished = 0
     
-    while finished < n:
-        while i<n and joblist[i][0]<=time:
-            req,dur, idx = joblist[i]
+    while finished < len(jobs):
+        while i<len(jobs) and joblist[i][0]<=time:
+            req, dur, idx = joblist[i]
             heapq.heappush(heap, (dur, req, idx))
             i+=1
         
-        if not heap:
-            time = joblist[i][0]
-            continue
+        if heap:
+            work, start, num = heapq.heappop(heap)
+            time+=work
+            answer += time - start
+            finished+=1
             
-        work, start, num = heapq.heappop(heap)
-        time+=work 
-        answer += time - start
-        finished+=1
+        else: time+=1
         
-    return answer//n
+    return answer//len(jobs)
