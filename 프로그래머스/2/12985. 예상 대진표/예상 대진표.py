5ddb3e13e9ff169e508fdc9b5019b634cdd21087
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-08-30 16:20:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,14 @@
+def solution(n,a,b):
+    answer = 0
+    
+    while a!=b:
+        if a%2==1:
+            a+=1
+            
+        if b%2==1:
+            b+=1
+        
+        a//=2
+        b//=2
+        answer+=1    
+    return answer
