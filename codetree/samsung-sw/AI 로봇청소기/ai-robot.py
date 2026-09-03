b6d0cf3b507b832b618843647f257eba5203c786
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-03 11:01:02 +0900

    [Passed] AI 로봇청소기 (278ms, 23MB)

--- codetree/samsung-sw/AI 로봇청소기/ai-robot.py
+++ codetree/samsung-sw/AI 로봇청소기/ai-robot.py
@@ -1,159 +1,129 @@
-import sys
 from collections import deque
+import sys
 
 N, K, L = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(N)]
-cleaner = [[] for _ in range(K)]
+grid = [ list(map(int, input().split())) for _ in range(N)]
+cleaner = [[0,0] for _ in range(K)]
 for i in range(K):
     r, c = map(int, input().split())
-    r=r-1; c=c-1
-    cleaner[i] = [r, c]
+    cleaner[i] = [r-1,c-1]
 
+dr = [-1,1,0,0]
+dc = [0,0,-1,1]
 
-def move_cleaner():
-    global grid
+# 청소기 이동
+# 청소기 좌표, 먼지 현황, 몇번째 청소기인지
+# 이동거리 계산, 가장 가까운 격자로 이동
+# 청소기의 좌표만 다시 계산해서 바뀌는 건 청소기 좌표뿐
+# 이동거리 짧은것 > 같다면 행작은거>열작은거
+def move(cleaner, grid, i):
+    sr, sc = cleaner[i]
+    q=deque([(sr, sc)])
+    visited=[[False]*N for _ in range(N)]
+    visited[sr][sc] = True
+    cleaner_set = set(map(tuple,cleaner))
+    candidates = []
+    dist = [[-1]*N for _ in range(N)]
+    dist[sr][sc]=0
+    while q:
+        cr, cc = q.popleft()
 
-    for i in range(K):
-        r, c = cleaner[i]
-        v=[[-1]*N for _ in range(N)]
-        if grid[r][c]>0:
-            continue
-        q=deque([(r,c)])
-        v[r][c]=0
-        min_dist = float('inf')
-        can = []
-        cleaner_set = set(tuple(pos) for pos in cleaner)
+        if grid[cr][cc]>0:
+            candidates.append((dist[cr][cc], cr, cc))
 
-        while q:
-            cr, cc = q.popleft()
-
-            if v[cr][cc]>min_dist:
-                break
-
-            if grid[cr][cc]>0:
-                if v[cr][cc]<=min_dist:
-                    min_dist = v[cr][cc]
-                    can.append([v[cr][cc], cr, cc])
-            
-            for dr, dc in ((-1, 0), (1, 0), (0,-1),(0,1)):
-                nr, nc = cr+dr, cc+dc
-                if 0<=nr<N and 0<=nc<N and grid[nr][nc]>=0 and v[nr][nc]==-1 and (nr, nc) not in cleaner_set:
-                    q.append((nr, nc))
-                    v[nr][nc] = v[cr][cc]+1
-        if can:
-            can.sort()
-            dist, new_r, new_c = can[0]
-            cleaner[i] = [new_r, new_c]
-
-
-# move_cleaner()
-# print(grid)
-# print(cleaner)
-
-def cleaning():
-    global grid
-
-    for r, c in cleaner:
-        order = [[[r+1, c], [r-1, c], [r, c+1], [r,c]]
-            ,[[r+1, c], [r, c-1], [r, c+1], [r,c]]
-            ,[[r+1, c], [r-1, c], [r, c-1], [r,c]]
-            ,[[r-1, c], [r, c-1], [r, c+1], [r,c]]
-            ]
-        final = []
         for d in range(4):
-            total=0
-            for i in range(4):
-                nr, nc = order[d][i]
-                if 0<=nr<N and 0<=nc<N and grid[nr][nc] >0:
-                    if grid[nr][nc] >=20:
-                        total+=20
-                    else:
-                        total +=grid[nr][nc]
-                    # print(total)
-            final.append((-total,d))
-        # print(r,c, "\n", final)
-        if final:
-            
-            final.sort()
-            # print(r,c, "\n", final)
-            _, dir = final[0]
-            # print(dir)
-            for j in range(4):
-                temp_r, temp_c = order[dir][j]
-                # print("test", r, c, temp_r, temp_c)
-                if 0<=temp_r<N and 0<=temp_c<N and grid[temp_r][temp_c] >0:
-                    # print("success")
-                    # print("pass", temp_r, temp_c)
-                    # print("ccc")
-                    if grid[temp_r][temp_c]>=20:
+            nr, nc = cr+dr[d], cc+dc[d]
+            if 0<=nr<N and 0<=nc<N and visited[nr][nc]==False and grid[nr][nc]!=-1 and (nr, nc) not in cleaner_set:
+                q.append((nr, nc))
+                visited[nr][nc]=True
+                dist[nr][nc]=dist[cr][cc]+1
+    if not candidates:
+        return
+    candidates.sort()
+    _, new_r, new_c = candidates[0]
+    cleaner[i] = [new_r, new_c]
 
-                        grid[temp_r][temp_c]-=20
-                    else:
-                        grid[temp_r][temp_c]=0
 
-# for row in grid: 
-#     print(row) 
-# print("\n") 
-# cleaning()
-# for row in grid: 
-#     print(row)    
-# print("\n") 
+def clean(cleaner, grid):
+    possible = []
+    nlist = []
+    cleaner_set = set(map(tuple, cleaner))
+    for r, c in cleaner:
+        # 오른쪽
+        for d in range(4):
+            total = 0
+            if d==0:
+                nlist = [(r, c+1),(r-1,c),(r+1,c),(r,c)]
+        # 아래쪽
+            if d==1:
+                nlist = [(r+1, c), (r, c-1), (r, c+1), (r, c)]
+        # 왼쪽
+            if d==2:
+                nlist = [(r, c - 1), (r - 1, c), (r + 1, c), (r, c)]
+        # 위쪽
+            if d==3:
+                nlist = [(r-1, c), (r, c-1), (r , c+1), (r, c)]
 
-# for row in cleaner:
-#     print(row)
+            for tempr, tempc in nlist:
+                if 0<=tempr<N and 0<=tempc<N and grid[tempr][tempc]>0:
+                    total += min(grid[tempr][tempc], 20)
+            possible.append((-total, d))
 
-def dirty():
-    global grid
+        finald = min(possible)[1]
+        # 오른쪽
+
+        if finald == 0:
+            nlist = [(r, c + 1), (r - 1, c), (r + 1, c), (r, c)]
+        # 아래쪽
+        elif finald == 1:
+            nlist = [(r + 1, c), (r, c - 1), (r, c + 1), (r, c)]
+        # 왼쪽
+        elif finald == 2:
+            nlist = [(r, c - 1), (r - 1, c), (r + 1, c), (r, c)]
+        # 위쪽
+        elif finald == 3:
+            nlist = [(r - 1, c), (r, c - 1), (r, c + 1), (r, c)]
+
+        for tempr, tempc in nlist:
+            if 0 <= tempr < N and 0 <= tempc < N and grid[tempr][tempc] > 0:
+                grid[tempr][tempc] = max(0, grid[tempr][tempc]-20)
+        possible = []
+
+
+def accumulate(grid):
     for r in range(N):
         for c in range(N):
             if grid[r][c]>0:
                 grid[r][c]+=5
-# dirty()
-# print(grid)
-def spread():
-    global grid
 
+def spread(grid):
     new_grid = [row[:] for row in grid]
-    for r in range(N):
+
+    for r  in range(N):
         for c in range(N):
             if grid[r][c]==0:
                 total = 0
-                for dr, dc in ((-1, 0), (1, 0), (0,-1),(0,1)):
-                    nr, nc = r+dr, c+dc
+                for d in range(4):
+                    nr, nc = r+dr[d], c+dc[d]
                     if 0<=nr<N and 0<=nc<N and grid[nr][nc]>0:
                         total+=grid[nr][nc]
                 new_grid[r][c] = total//10
-    grid = new_grid
+    return new_grid
 
-# spread()
-# print(grid)
+def main(grid, cleaner):
+    for _ in range(L):
+        for i in range(K):
+            move(cleaner, grid, i)
 
-def score():
-    total =0
-    for r in range(N):
-        for c in range(N):
-            if grid[r][c]>0:
-                total+=grid[r][c]
-    print(total)
+        clean(cleaner, grid)
+        accumulate(grid)
+        grid = spread(grid)
 
+        total = 0
+        for r in range(N):
+            for c in range(N):
+                if grid[r][c]>0:
+                    total+=grid[r][c]
 
-
-for _ in range(L):
-    move_cleaner()
-    # for row in grid: 
-    #     print(row) 
-    # for c in cleaner: 
-    #     print(c) 
-    cleaning()
-    # for row in grid: 
-    #     print(row) 
-    # for c in cleaner: 
-    # #     print(c) 
-    dirty()
-    spread()
-    # # for row in grid: 
-    # #     print(row) 
-    # # for c in cleaner: 
-    # #     print(c) 
-    score()
-
+        print(total)
+main(grid, cleaner)
