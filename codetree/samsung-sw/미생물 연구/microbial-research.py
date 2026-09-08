ee7ea02b07cab41997d2b1d6e3ca7af9d338acfd
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-08 11:06:39 +0900

    [Passed] 미생물 연구 (232ms, 24MB)

--- codetree/samsung-sw/미생물 연구/microbial-research.py
+++ codetree/samsung-sw/미생물 연구/microbial-research.py
@@ -1,155 +1,127 @@
-import sys
-from collections import deque
-from collections import Counter
+from collections import deque, Counter
+# 미생물 투입
+# 덮어씌기, 둘 이상 나눠지면 사라짐
 
-# 입력 받기
+# 배양 용기 이동
+# 영역 넓은 무리 > 먼저 투입된거 > 다른 미생물과 겹치지 않게, 범위 벗어나지 않게, x 최소 > y 최소, 둘 수 없는 미생물은 사라짐
+
+# 실험 결과 기록
+# 인접한 물리 쌍, A 넓이 * B 넓이
+
 N, Q = map(int, input().split())
+cells = {}
+board = [[-1]*N for _ in range(N)]
+dr = [-1,1,0,0]
+dc = [0,0,-1,1]
 
-# 자료형
-grid = [[-1]*(N) for _ in range(N)]
-bio = {}
+for k in range(Q):
+    r1,c1,r2,c2 = map(int, input().split())
+    cells[k] = [r1,c1,r2,c2]
 
+def add_cell(k):
+    r1,c1,r2,c2 = cells[k]
+    for i in range(r1, r2):
+        for j in range(c1, c2):
+            board[i][j]=k
 
-def print_grid(grid):
+def delete_if_seperate(board):
+    visited = [[False] * N for _ in range(N)]
+    group = []
     for i in range(N):
-        print(*grid[i], sep = '\t')
+        for j in range(N):
+            if board[i][j] !=-1 and not visited[i][j]:
+                key = board[i][j]
+                group.append(key)
+                q = deque([(i, j)])
+                visited[i][j] = True
+                while q:
+                    r, c = q.popleft()
+                    for d in range(4):
+                        nr, nc = r + dr[d], c + dc[d]
+                        if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc]:
+                            if board[nr][nc] == key:
+                                visited[nr][nc]=True
+                                q.append((nr, nc))
 
+    counts = Counter(group)
+    candidate={k for k, cnt in counts.items() if cnt >1}
 
-def adding(i):
-    sj, si, ej, ei = map(int, input().split())
-    bio[i] = [si, sj, ei, ej] # row 먼저, col 두번째로 배치
-    for r in range(si, ei):
-        for c in range(sj, ej):
-            grid[r][c] = i
+    # 한개 이상이면 삭제
+    # k였던 좌표들 모두 -1로 채우기
+    if candidate:
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] in candidate:
+                    board[i][j] = -1
 
-# 구역별로 section에 [key, [r,c], [r,c] .. ] 하는 bfs
-def bfs(r, c, v, grid):
-    # bfs에 튜플을 넣을지, 리스트를 넣을지
-    q = deque([(r, c)])
-    v[r][c] = True
-    section =[grid[r][c], [r, c]]
+# 이동 순서 결정
+def order():
+    visited=[[-1]*N for _ in range(N)]
+    ordered = []
+    positions = {}
 
-    while q:
-        cr, cc = q.popleft()
-        for (dr, dc) in ((-1,0), (1,0), (0,-1), (0,1)):
-            nr, nc = cr+dr, cc+dc
-            # 범위 안, 키가 같, 방문 안함
-            if 0<=nr<N and 0<=nc<N and v[nr][nc] is False and grid[nr][nc] == grid[r][c]:
-                q.append([nr, nc])
-                v[nr][nc] = True
-                section.append(([nr, nc]))
-    return section
+    for i in range(N):
+        for j in range(N):
+            key = board[i][j]
+            if key!=-1:
+                if key not in positions:
+                    positions[key] =[]
+                positions[key].append((i, j))
 
-# 쪼개진지 검사하는게 필요, bfs는 따른 함수로 빼두기
-def inspecting(): 
-    v = [[False]*N for _ in range(N)]
-    section_list = []
+    ordered = sorted(positions, key = lambda k: (-len(positions[k]),k))
+    return ordered, positions
 
-    # grid 변경되었으니, bfs 돌리면서 bio 별로 좌표 리스트 추가해주고, 만약 쪼개지면 아예 없애주는거
-    for r in range(N):
-        for c in range(N):   
-            if grid[r][c]>0 and v[r][c] is False:
-                
-                section = bfs(r, c, v, grid)
-                section_list.append(section)
+def move_cell():
+    new_board= [[-1]*N for _ in range(N)]
+    ordered, positions = order()
+    for key in ordered:
+        coords = positions[key]
+        min_r = min(r for r, c in coords)
+        min_c = min(c for r, c in coords)
 
-    
+        shape = [(r-min_r, c-min_c) for r, c in coords]
 
-    # for arr in section_list:
-    #     for brr in section_list:
-    #         # arr이 brr이랑 다르고, 키가 같을때
-    #         if arr != brr and arr[0]==brr[0]:
-    #             rem_set.add(arr[0])
+        r_size = max(r for r, c in shape) +1
+        c_size = max(c for r, c in shape) +1
+        placed = False
+        for r in range(N-r_size+1):
+            for c in range(N-c_size+1):
+                can_place = True
+                for rr, cc in shape:
+                    if new_board[r+rr][c+cc] !=-1:
+                        can_place=False
+                        break
+                if can_place:
+                    for rr, cc in shape:
+                        new_board[r+rr][c+cc] = key
+                    placed = True
+                    break
+            if placed:
+                break
+    return new_board
 
-    rem_set= set()
-    cnt = Counter(arr[0] for arr in section_list)
-    rem_set = {key for key, c in cnt.items() if c>1}
-
-    glist = [arr for arr in section_list if arr[0] not in rem_set]
-
-    for arr in section_list:
-        if arr[0] in rem_set:
-            for r, c in arr[1:]:
-                grid[r][c]= -1
-
-    return glist
-
-def move(glist):
-    new_list = []
-# 원점좌표
-    for arr in glist:
-        min_r = min(r for r, c in arr[1:])
-        min_c = min(c for r, c in arr[1:])
-        for i  in range(1, len(arr)):
-            arr[i][0]-=min_r
-            arr[i][1]-=min_c
-        new_list.append(arr)
-        
-# 영역, 먼저투입(키 작은) 순서대로 배치
-    new_list.sort(key=lambda x: (-len(x[1:]), x[0]))
-
-# 차례대로 이동, 모든 영역이 범위 내, 그리드이 0보다 크지 않으면 
-    new_grid = [[0]*N for _ in range(N)]
-    final_list = []
-
-    for arr in new_list:
-        is_placed = False
-        key=arr[0]
-        shape = arr[1:]
-        sr, sc = find_position(shape, new_grid)
-        if sr is not None:
-            for r, c in shape:
-                new_grid[r+sr][c+sc] = key
-            final_list.append(arr)
-    return new_grid ,final_list
-
-def find_position(shape, new_grid):
-    # 순서 바꿔서, 문제에서 col 작은거 > row 작은거 순으로 배치
-    # 탐색 순서가 정렬 순서랑 같다!
-    for sc in range(N):
-        for sr in range(N):
-            # 모든 좌표가 유효한지 검사할 때
-            if all(0<=r+sr<N and 0<=c+sc<N and new_grid[r+sr][c+sc] == 0 for r, c in shape):
-                return sr, sc
-    return None, None
-    
-
-
-def score(new_grid, final_list):
-    area_dict ={}
-    for arr in final_list:
-        area_dict[arr[0]] = len(arr)-1
-    pairs = set()
+def calculate_score():
+    area = [0]*Q
+    pairs=set()
     for r in range(N):
         for c in range(N):
-            if new_grid[r][c]>0:
-                curr_id = new_grid[r][c]
-                if r+1<N:
-                    if new_grid[r+1][c] >0 and new_grid[r+1][c]!=curr_id:
-                        pairs.add((min(new_grid[r+1][c], curr_id), max(new_grid[r+1][c], curr_id)))
-                
-                if c+1<N:
-                    if new_grid[r][c+1] >0 and new_grid[r][c+1]!=curr_id:
-                        pairs.add((min(new_grid[r][c+1], curr_id), max(new_grid[r][c+1], curr_id)))
-                
-    total_score = 0
-    for id1, id2 in pairs:
-        total_score+=area_dict[id1] * area_dict[id2]
-    return total_score
+            key = board[r][c]
+            if key!=-1:
+                area[key]+=1
+                for d in range(4):
+                    nr, nc = r+dr[d], c+dc[d]
+                    if 0<=nr<N and 0<=nc<N and board[nr][nc]!=-1 and board[nr][nc]!=key:
+                        pairs.add((min(key, board[nr][nc]), max(key, board[nr][nc])))
+    score= 0
+    for a,b in pairs:
+        score += area[a]*area[b]
+    return score
 
 
+for k in range(Q):
+    add_cell(k)
+    delete_if_seperate(board)
+    board = move_cell()
+    print(calculate_score())
 
 
-for i in range(1,Q+1):
-    # 미생물 투입, 둘 이상 나눠지면 해당 미생물은 사라짐
-    adding(i)
-    glist = inspecting()
-
-    # 이동, 영역 넓은 > 먼저 투입 순서로 이동, 배양 용기 안벗어나고, 다른 미생물과 안겹치고 x 좌표 최소 > y좌표 최소, 불가능한 미생물은 삭제
-    grid, final_list = move(glist)
-
-    # 결과 기록, 맞닿으면 각 영역 곱만큼 성과
-    sc = score(grid, final_list)
-
-    print(sc)
-    
