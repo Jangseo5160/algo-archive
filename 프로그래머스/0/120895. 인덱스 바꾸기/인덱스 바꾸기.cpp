1ce2c01315677d1693e033c7eef981e7ec601a11
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-23 21:19:02 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int num1, int num2) {
+    string answer = "";
+    for(int i=0; i<my_string.size();i++){
+        if(i==num2){
+            answer+=my_string[num1];
+        }
+        else if (i==num1){
+            answer+=my_string[num2];
+        }
+        else{
+            answer+=my_string[i];
+        }
+    }
+    return answer;
+}
