1088b6fb546364c550408d02782a5104f5b2ca40
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-23 23:16:08 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
@@ -1,18 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
 int solution(vector<int> array, int n) {
-    int answer = 0;
-    sort(array.begin(), array.end());
-    vector<int> temp = array;
-    for(auto& a:temp){
-        a-=n;
-        if(a<0) a*=-1;
+    int answer = array[0];
+    
+    for(int a:array){
+        int dist = abs(a-n);
+        int min_dist = abs(answer-n);
+        if(dist<min_dist){
+            answer=a;
+        }
+        else if(dist==min_dist && a<answer){
+            answer=a;
+        }
     }
-    auto p = min_element(temp.begin(), temp.end()) - temp.begin();
-    answer = array[p];
+    
     return answer;
 }
