0a2a30c80aad5a96c096d4ea10664c72843f2f84
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-23 20:28:23 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    for(auto s:my_string){
+        if(isdigit(s)){
+            answer+=s-'0';
+        }
+    }
+    return answer;
+}
