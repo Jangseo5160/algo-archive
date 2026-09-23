d8100e0b1c8dbea3866287a4938136f38abc4d2b
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-23 16:15:24 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    while (order>0){
+        int  n = order%10;
+        if (n==3 || n==6 || n==9)  answer++;
+        order /= 10;
+    }
+    return answer;
+}
