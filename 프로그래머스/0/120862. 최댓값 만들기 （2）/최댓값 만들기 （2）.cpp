9b427f9537ed3d2ac64eda799e979f441c847632
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-23 16:10:39 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -1,31 +1,12 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int solution(vector<int> numbers) {
     int answer = 0;
-    vector<int> plus;
-    vector<int> minus;
-    for (auto n:numbers){
-        if (n>=0) plus.push_back(n);
-        else minus.push_back(n);
-    }
-    sort(plus.rbegin(), plus.rend());
-    sort(minus.begin(), minus.end());
-    if(minus.size()>=2 && plus.size()>=2){
-        int temp1 = minus[0]*minus[1];
-        int temp2 = plus[0]*plus[1];
-        if (temp1>temp2) return temp1;
-        else return temp2;
-    }
-    else if (minus.size()>=2){
-        return minus[0]*minus[1];
-    }
-    else if (plus.size()>=2){
-        return plus[0]*plus[1];
-    }
-    else return plus[0] * minus[0];
+    sort(numbers.begin(), numbers.end());
+    int n=numbers.size();
+    answer = max(numbers[0]*numbers[1], numbers[n-1]*numbers[n-2]);
     return answer;
 }
