95a172927a2ca5204153b13981e03a2eb6b661d4
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-23 23:34:08 +0900

    [level 0] Title: 소인수분해, Time: 0.02 ms, Memory: 4.61 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -3,23 +3,16 @@
 
 using namespace std;
 
-bool isPrime(int a){
-    if(a<2) return false;
-    for (int i=2; i*i<=a; i++){
-        if(a%i==0) return false;
-    }
-    return true;
-}
-
 vector<int> solution(int n) {
     vector<int> answer;
-    vector<int> temp;
-
-    for(int i=2; i<=n; i++){
-        if(n%i==0) temp.push_back(i);
-    }
-    for(int a:temp){
-        if (isPrime(a)) answer.push_back(a);
+    for(int i=2; i<=n;i++){
+        if(n%i==0){
+            answer.push_back(i);
+            while(n%i==0){
+                n/=i;
+            }
+        }
+        
     }
     return answer;
 }
