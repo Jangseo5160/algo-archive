c9de641ad5c2dfb62236d13e75df58dc996962cd
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-23 14:26:11 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -5,9 +5,6 @@ using namespace std;
 
 int solution(int slice, int n) {
     int answer = 0;
-    for (auto i=1; i<100; i++){
-        if(slice*i>=n) return i;
-        
-    }
-    // return answer;
+    answer = (slice+n-1)/slice;
+    return answer;
 }
