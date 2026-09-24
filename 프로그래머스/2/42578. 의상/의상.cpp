f99d99148a3488ae009ce96f37a5275fd2a8e93e
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-24 11:50:24 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 4.66 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map <string, int> mp;
+    for(int i=0; i<clothes.size(); i++){
+        string category = clothes[i][1];
+        string cloth = clothes[i][0];
+        mp[category]++;
+    }
+    for(auto a:mp){
+        answer *= (a.second+1);
+    }
+    return answer-1;
+}
