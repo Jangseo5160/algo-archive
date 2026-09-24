82e58b55ebd328b2c803a478a662e8b5e2dce95a
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-25 00:56:41 +0900

    [level 2] Title: 주식가격, Time: 22.68 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,20 +1,24 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
-    for(int i=0; i<prices.size()-1; i++){
-        int cnt=0;
-        for(int j=i; j<prices.size()-1; j++){
-            if(prices[i]<=prices[j]){
-                cnt++;
-            }
-            else break;
+    vector<int> answer(prices.size(), 0);
+    stack<int> s;
+    for(int i=0; i<prices.size(); i++){
+        while(!s.empty() && prices[s.top()]>prices[i]){
+            int idx = s.top();
+            s.pop();
+            answer[idx] = i-idx;
         }
-        answer.push_back(cnt);
+        s.push(i);
     }
-    answer.push_back(0);
+    while(!s.empty()){
+        int idx = s.top();
+        s.pop();
+        answer[idx] = prices.size()-1-idx;
+    }
     return answer;
 }
