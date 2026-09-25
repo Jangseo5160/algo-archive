0b88028c48c099b1910c21a284d60da7a676eb9c
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-26 00:13:13 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.09 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 int solution(int n, vector<vector<int>> wires) {
@@ -10,36 +11,34 @@ int solution(int n, vector<vector<int>> wires) {
         graph[a[0]].push_back(a[1]);
         graph[a[1]].push_back(a[0]);
     }
-    for(auto a: wires){
+
+
+    for(auto a:wires){
         int cut_a=a[0];
         int cut_b=a[1];
-        vector<bool> visited(n+1, false);
         queue<int> q;
-        
+        vector<bool> visited(n+1);
         q.push(1);
         visited[1]=true;
-        
         int cnt=0;
         
-
-        while (!q.empty()){
-            auto cur = q.front();
+        while(!q.empty()){
+            int curr = q.front();
             q.pop();
             cnt++;
-            
-            for(auto next: graph[cur]){
-                if((cur==cut_a && next==cut_b)|| cur==cut_b && next==cut_a){
+
+            for(auto next: graph[curr]){
+                if((curr==cut_a && next==cut_b)|| (curr==cut_b && next==cut_a)){
                     continue;
                 }
-                if(!visited[next]){ 
+                if(!visited[next]){
                     q.push(next);
                     visited[next]=true;
                 }
             }
         }
         int other = n-cnt;
-        answer= min(answer, abs(cnt-other));
+        answer = min(answer, abs(cnt-other));
     }
-    
     return answer;
 }
