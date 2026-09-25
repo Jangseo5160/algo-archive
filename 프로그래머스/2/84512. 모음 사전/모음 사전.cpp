380d47046257d8adf8ef21817e53a37925c9a68d
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-25 19:38:10 +0900

    [level 2] Title: 모음 사전, Time: 0.20 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+vector<string> alpha = {"A", "E", "I", "O", "U"};
+int answer = 0;
+int cnt =0;
+
+void dfs(string curr, const string& word ){
+    if(curr.size()>5){
+        return;
+    }
+    if(!curr.empty()){
+        cnt++;
+        if(curr == word){
+            answer=cnt;
+            return;
+        }
+    }
+
+    for(auto a: alpha){
+        dfs(curr+a, word);
+        if(answer!=0) return;
+    }
+}
+
+
+int solution(string word) {
+    dfs("", word);
+    
+    return answer;
+}
