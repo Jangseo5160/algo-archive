0abd6ebaaf65d9fef2e59003516cc9ed85f6fa72
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-26 14:22:56 +0900

    [level 2] Title: 구명보트, Time: 1.46 ms, Memory: 5.12 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,33 +1,27 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int solution(vector<int> people, int limit) {
-    vector<int> cnt(241, 0);
-    for(int x: people){
-        cnt[x]++;
-    }
-    int left =0;
-    int right=240;
-    int answer=0;
-    
-    while (left<=right){
-        while(left<=right && cnt[left]==0){
-            left++;
-        }
-        while (left<=right && cnt[right]==0){
-            right--;
-        }
-        if(left>right){
+    int answer = 0;
+    sort(people.begin(), people.end());
+    int left = 0;
+    int right = people.size()-1;
+    while(left<=right){
+        if(left==right){
+            answer++;
             break;
         }
-        cnt[right]--;
-        answer++;
-        if(left<=right && left+right<=limit && cnt[left]>0){
-            cnt[left]--;
+        if(people[left]+people[right]<=limit){
+            answer++;
+            left++;
+            right--;
         }
-        
+        else{
+            answer++;
+            right--;
+        }
     }
     return answer;
 }
