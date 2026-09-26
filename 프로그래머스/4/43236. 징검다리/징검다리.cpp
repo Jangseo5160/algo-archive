e794393033dc5e0f09505a868dbe9451aac39804
Jangseo5160 <121782684+Jangseo5160@users.noreply.github.com>
2026-09-26 17:06:00 +0900

    [level 4] Title: 징검다리, Time: 4.38 ms, Memory: 5.39 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    long long answer = 0;
+    long long left = 0;
+    long long right = distance;
+    
+    rocks.push_back(distance);
+    sort(rocks.begin(), rocks.end());
+    
+    while(left<=right){
+        long long mid = (left+right)/2;
+        long long cnt=0;
+        long long prev = 0;
+        
+        for(int i=0; i<rocks.size(); i++){
+            if((rocks[i]-prev)<mid){
+                cnt++;
+            }
+            else{
+                prev = rocks[i];
+            }
+        }
+        if(cnt<=n){
+            answer=mid;
+            left=mid+1;
+
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    return answer;
+}
